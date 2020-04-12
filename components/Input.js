a6fd5eeffc760d547c4e73083a1b9e13f2ce6710
Mohamed Derhalli <derhallim@gmail.com>
2020-04-12 05:15:18 -0400

    remove unnecessary styles in input component

--- components/Input.js
+++ components/Input.js
@@ -3,16 +3,14 @@ import { StyleSheet, TextInput } from 'react-native'
 
  const Input = (props) => {
     return (
-        <TextInput {...props} style={{...styles.input, ...props.style}}></TextInput>
+        <TextInput {...props} style={{...styles.input}}></TextInput>
     )
 }
 
 const styles = StyleSheet.create({
 input: {
     height: 30,   
-    borderBottomColor: 'grey',
-    borderBottomWidth: 1, 
-    marginVertical: 10
+    
 }
 })
 
